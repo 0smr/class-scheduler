cdd577b1dc18a61bd8df48354de1cc2bb09ee47e
seyyed morteza razavi <seyyedmortezarazavi76@gmail.com>
2020-05-10 10:07:37 +0430

    some changes

--- course.cpp
+++ course.cpp
@@ -3,91 +3,95 @@
 //static variable initializing
 unsigned long long int course::counter{};
 
+course::course(): QObject(nullptr)
+  , unit(1), ID(counter++)
+{}
+
 course::course(QObject *parent) : QObject(parent)
   , unit(1), ID(counter++)
 {
-	profID = nullptr;
-	generationSL = nullptr;
-	name = "no name";
+    profID = nullptr;
+    generationSL = nullptr;
+    name = "no name";
 }
 
 course::course(QString name, const unsigned int *pID, QObject *parent):course(parent)
 {
-	this->name = name;
-	this->profID = pID;
+    this->name = name;
+    this->profID = pID;
 }
 
 course::course(QString name, unsigned int *pID,const  QVector<unsigned short> * tSL
-			   , unsigned short **, QObject *parent)
-	:course(name ,pID ,parent)
+               , unsigned short **, QObject *parent)
+    :course(name ,pID ,parent)
 {
-	this->timetableSL = tSL;
+    this->timetableSL = tSL;
 }
 
 void course::addConstraintSubSet(QVector<course *> *limiterList)
 {
-	constraintSetSL.push_back(limiterList);
+    constraintSetSL.push_back(limiterList);
 }
 
 course * const *course::find(const QVector<course *> &container, const QString &val)
 {
-	for(auto x = container.begin() ; x!=container.end() ; ++x)
-		if((*x)->getName() == val)
-			return x;
-	return nullptr;
+    for(auto x = container.begin() ; x!=container.end() ; ++x)
+        if((*x)->getName() == val)
+            return x;
+    return nullptr;
 }
 
 void course::setTimetableSL(const QVector<unsigned short> * tSL)
 {
-	this->timetableSL = tSL;
+    this->timetableSL = tSL;
 }
 
 void course::setGenerationSL(unsigned short *** gSL)
 {
-	generationSL = gSL;
+    generationSL = gSL;
 }
 
 void course::setUnit(const unsigned short & un)
 {
-	unit = un;
+    unit = un;
 }
 
 void course::setColumn(unsigned int col)
 {
-	column = col;
+    column = col;
 }
 
 const QString &course::getName() const
 {
-	return name;
+    return name;
 }
 
 long long course::getCurrentFitness(unsigned int gen,int EConflict, int IConflict) const
 {
-	//equal to long long
-	long long int fitness{};
+    //equal to long long
+    long long int fitness{};
 
-	for(int i = 0; i < constraintSetSL.size() ; ++i)
-		for(int j = 0; j < constraintSetSL[i]->size() ;++j)
-			if(constraintSetSL[i]->at(j)->ID != ID &&
-					(*generationSL)[gen][constraintSetSL[i]->at(j)->column] == (*generationSL)[gen][column])
-				fitness += EConflict;
+    for(int i = 0; i < constraintSetSL.size() ; ++i)
+        for(int j = 0; j < constraintSetSL[i]->size() ;++j)
+            if(constraintSetSL[i]->at(j)->ID != ID &&
+                    (*generationSL)[gen][constraintSetSL[i]->at(j)->column] == (*generationSL)[gen][column])
+                fitness += EConflict;
 
-	for(int i = 0; i < professorCoursesLS->size() ; ++i)
-		if((*professorCoursesLS)[i]->ID != ID &&
-					(*generationSL)[gen][(*professorCoursesLS)[i]->column] == (*generationSL)[gen][column])
-			fitness += IConflict;
+    for(int i = 0; i < professorCoursesLS->size() ; ++i)
+        if((*professorCoursesLS)[i]->ID != ID &&
+                    (*generationSL)[gen][(*professorCoursesLS)[i]->column] == (*generationSL)[gen][column])
+            fitness += IConflict;
 
-	return fitness;
+    return fitness;
 }
 
 const QVector<unsigned short> *course::getTimetableSL() const
 {
-	return timetableSL;
+    return timetableSL;
 }
 
 void course::setProfessorCoursesLS(const QVector<course *> *value)
 {
-	professorCoursesLS = value;
+    professorCoursesLS = value;
 }
 
--- course.h
+++ course.h
@@ -7,46 +7,46 @@
 
 class course : public QObject
 {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	//------------------ special member function ------------------
-	explicit course() = default;
-	explicit course(QObject *parent = nullptr);
+    //------------------ special member function ------------------
+    explicit course();
+    explicit course(QObject *parent = nullptr);
 
-	course(QString name  ,const unsigned int* pID = nullptr, QObject *parent = nullptr);
-	course(QString name  , unsigned int* pID , const QVector<unsigned short> *
-			 , unsigned short int ** =nullptr , QObject *parent = nullptr);
+    course(QString name  ,const unsigned int* pID = nullptr, QObject *parent = nullptr);
+    course(QString name  , unsigned int* pID , const QVector<unsigned short> *
+             , unsigned short int ** =nullptr , QObject *parent = nullptr);
 
-	//------------------ common member function ------------------
-	// add constraint subset to CSSLList
-	void addConstraintSubSet(QVector<course *> *);
-	static course * const *find(const QVector<course *> &container, const QString &val);
+    //------------------ common member function ------------------
+    // add constraint subset to CSSLList
+    void addConstraintSubSet(QVector<course *> *);
+    static course * const *find(const QVector<course *> &container, const QString &val);
 
-	//------------------ setter member function ------------------
-	void setTimetableSL(const QVector<unsigned short int> *);
-	void setProfessorCoursesLS(const QVector<course *> *);
-	void setGenerationSL(unsigned short int *** );
-	void setUnit(const unsigned short &);
-	void setColumn(unsigned int );
+    //------------------ setter member function ------------------
+    void setTimetableSL(const QVector<unsigned short int> *);
+    void setProfessorCoursesLS(const QVector<course *> *);
+    void setGenerationSL(unsigned short int *** );
+    void setUnit(const unsigned short &);
+    void setColumn(unsigned int );
 
-	//------------------ getter member function ------------------
-	const QString &getName()const;
-	//(generation indexNumber, External Conflict Value, Internal Conflict Value)
-	long long getCurrentFitness(unsigned int , int = 9 , int = 12)const;
-	const QVector<unsigned short int> * getTimetableSL() const;
+    //------------------ getter member function ------------------
+    const QString &getName()const;
+    //(generation indexNumber, External Conflict Value, Internal Conflict Value)
+    long long getCurrentFitness(unsigned int , int = 9 , int = 12)const;
+    const QVector<unsigned short int> * getTimetableSL() const;
 
-	//------------------ constexpr member function ------------------
-	// return size of timetableSL
-	constexpr int getTimetableSLSize() const
-	{
-		return timetableSL->size();
-	}
+    //------------------ constexpr member function ------------------
+    // return size of timetableSL
+    constexpr int getTimetableSLSize() const
+    {
+        return timetableSL->size();
+    }
 
-	// return size of constraintSetSL
-	constexpr int getCSSLSize() const
-	{
-		return constraintSetSL.size();
-	}
+    // return size of constraintSetSL
+    constexpr int getCSSLSize() const
+    {
+        return constraintSetSL.size();
+    }
 
 
 signals:
@@ -54,35 +54,35 @@ signals:
 public slots:
 
 private:
-	//------------------ list member ------------------
-	QString name;
+    //------------------ list member ------------------
+    QString name;
 
-	//QString prof.Name
+    //QString prof.Name
 
-	// constraint set symbolic link
-	QVector<QVector<course *> *> constraintSetSL;
-	// timetable symbolic link (number of days that a master can present)
-	const QVector<unsigned short int> *timetableSL;
-	const QVector<course *> *professorCoursesLS;
+    // constraint set symbolic link
+    QVector<QVector<course *> *> constraintSetSL;
+    // timetable symbolic link (number of days that a master can present)
+    const QVector<unsigned short int> *timetableSL;
+    const QVector<course *> *professorCoursesLS;
 
-	//------------------ common member ------------------
-	/*
-	 * column index in generation table.
-	 * can equal to ID.
-	 */
-	unsigned int column;
+    //------------------ common member ------------------
+    /*
+     * column index in generation table.
+     * can equal to ID.
+     */
+    unsigned int column;
 
-	// hour in week, a number between 1 - 72
-	short int hour;
-	unsigned short int unit;
+    // hour in week, a number between 1 - 72
+    short int hour;
+    unsigned short int unit;
 
-	//------------------ pointer member ------------------
-	const unsigned int * profID;
-	unsigned short int *** generationSL;
-	//------------------ const member ------------------
-	const unsigned long long int ID;
+    //------------------ pointer member ------------------
+    const unsigned int * profID;
+    unsigned short int *** generationSL;
+    //------------------ const member ------------------
+    const unsigned long long int ID;
 
-	//------------------ static member ------------------
-	static unsigned long long int counter;
+    //------------------ static member ------------------
+    static unsigned long long int counter;
 };
 #endif // COURSE_H
